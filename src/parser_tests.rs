2631c34c34cc94094404958940c8a362c53c9ba1
Random_Civvy <vomalexander@gmail.com>
2017-10-07 18:05:01 -0700

    Add parser test for function declaration

--- src/parser_tests.rs
+++ src/parser_tests.rs
@@ -1,1 +1,10 @@
+#[cfg(test)]
+mod parser_tests {
+    use parser::*;
 
+    #[test]
+    fn test_parse_FnDecl() {
+        let input = "fn test_fn(i32 arg)";
+        parse_FnDecl(input).unwrap();
+    }
+}
