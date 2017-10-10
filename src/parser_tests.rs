104ee8a5b16a24d2756843fe83a2f05918b2572a
Random_Civvy <vomalexander@gmail.com>
2017-10-09 19:15:42 -0700

    test_parse_FnDecl checks for expected Function struct

--- src/parser_tests.rs
+++ src/parser_tests.rs
@@ -1,10 +1,16 @@
 #[cfg(test)]
 mod parser_tests {
+    use std::str::FromStr;
     use parser::*;
+    use ascii::*;
+    use ast::*;
 
     #[test]
     fn test_parse_FnDecl() {
         let input = "fn test_fn(i32 arg) { }";
-        parse_FnDecl(input).unwrap();
+        let func = parse_FnDecl(input).unwrap();
+        assert_eq!(func.name, Ident::new("test_fn"));
+        assert_eq!(func.args, Some(vec![FnArg { name: Ident::new("arg"), arg_type: Ident::new("i32")}]));
+        assert_eq!(func.body, AstNode::untyped(Block(Vec::new())));
     }
 }
