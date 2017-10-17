227ef236b5a0d629ade5e7bf2de98f60a0e8bd92
Random_Civvy <vomalexander@gmail.com>
2017-10-16 17:10:10 -0700

    Move ast node macros under lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,74 @@
 extern crate itertools;
 extern crate ascii;
 
+macro_rules! boolean {
+    ($val: expr) => {{
+        Literal::Bool($val)
+    }};
+
+    ($val: expr => Expr) => {{
+        Expr::Literal(AstNode::typed(boolean!($val), SmplType::Bool))
+    }};
+
+    ($val: expr => BoxExpr) => {{
+        Box::new(boolean!($val => Expr))
+    }}
+}
+
+macro_rules! number {
+    ($str_num: expr) => {{
+        Literal::Number($str_num.to_string())
+    }};
+
+    ($str_num: expr => Expr) => {{
+        Expr::Literal(AstNode::untyped(number!($str_num)))
+    }};
+
+    ($str_num: expr => BoxExpr) => {{
+        Box::new(number!($str_num => Expr))
+    }}
+}
+
+macro_rules! ident {
+    ($ident: expr) => {{
+        Ident(AsciiString::from_str($ident).unwrap())
+    }};
+
+    ($ident: expr => Expr) => {{ 
+        Expr::Ident(AstNode::untyped(ident!($ident)))
+    }};
+
+    ($ident: expr => BoxExpr) => {{
+        Box::new(ident!($ident => Expr))
+    }}
+}
+
+macro_rules! path {
+    ($($segment: expr),*) => {{
+        let mut v = Vec::new();
+        $(v.push(ident!($segment));)*;
+        Path(v)
+    }};
+}
+
+macro_rules! bin_expr {
+    ($lhs: expr, $op: expr, $rhs: expr) => {{
+        BinExpr {
+            op: $op,
+            lhs: AstNode::untyped($lhs),
+            rhs: AstNode::untyped($rhs),
+        }
+    }};
+
+    (($lhs: expr, $op: expr, $rhs: expr) => Expr) => {{
+        Expr::Bin(AstNode::untyped(bin_expr!($lhs, $op, $rhs)))
+    }};
+
+    (($lhs: expr, $op: expr, $rhs: expr) => BoxExpr) => {{
+        Box::new(bin_expr!(($lhs, $op, $rhs) => Expr))
+    }};
+}
+
 mod parser;
 mod ast;
 mod smpl_type;
