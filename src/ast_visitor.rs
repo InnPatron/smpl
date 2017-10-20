bfd8f98d4f7e9b50fe3e1f5a687228f8156181e7
Random_Civvy <vomalexander@gmail.com>
2017-10-20 12:27:10 -0700

    Remove Visitable trait and make functions inherent to create more flexilibilty

--- src/ast_visitor.rs
+++ src/ast_visitor.rs
@@ -1,13 +1,10 @@
 use ast::*;
 use semantic_ck;
 
-pub trait Visitable {
-    fn visit(&mut self, 
-                 checker: &mut semantic_ck::SemanticChecker) -> Result<(), semantic_ck::Err>;
-}
 
-impl Visitable for Program {
-    fn visit(&mut self, 
+
+impl Program {
+    pub fn visit(&mut self, 
                  checker: &mut semantic_ck::SemanticChecker) -> Result<(), semantic_ck::Err> {
         for decl_stmt in self.0.iter_mut() {
             decl_stmt.visit(checker)?;
@@ -16,28 +13,14 @@ impl Visitable for Program {
     }
 }
 
-impl Visitable for DeclStmt {
-    fn visit(&mut self, 
+impl DeclStmt {
+    pub fn visit(&mut self, 
                  checker: &mut semantic_ck::SemanticChecker) -> Result<(), semantic_ck::Err> {
         match *self {
-            DeclStmt::Struct(ref mut struct_def) => unimplemented!(),
-            DeclStmt::Function(ref mut fn_def) => unimplemented!(),
+            DeclStmt::Struct(ref mut struct_def) => { checker.accept_struct_def(struct_def)?; },
+            DeclStmt::Function(ref mut fn_def) => { checker.accept_fn_def(fn_def)?; },
         }
 
         Ok(())
-    }
-}
-
-impl Visitable for Struct {
-    fn visit(&mut self, 
-                 checker: &mut semantic_ck::SemanticChecker) -> Result<(), semantic_ck::Err> {
-        checker.accept_struct_def(self)
-    }
-}
-
-impl Visitable for Function {
-    fn visit(&mut self, 
-                 checker: &mut semantic_ck::SemanticChecker) -> Result<(), semantic_ck::Err> {
-        checker.accept_fn_def(self)
     }
 }
