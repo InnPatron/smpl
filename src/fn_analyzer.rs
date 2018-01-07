e9544f15e8dc9b72366b051a54ba400a9624249c
Random_Civvy <vomalexander@gmail.com>
2018-01-06 23:44:13 -0700

    Prototype analyze_fn()

--- src/fn_analyzer.rs
+++ src/fn_analyzer.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use control_flow::*;
 use typed_ast::*;
 use semantic_ck::{Universe, ScopedData, Err};
@@ -10,13 +12,43 @@ pub enum TypeErr {
 
 }
 
-pub fn analyze_fn(global_scope: &ScopedData, fn_type: &FunctionType, cfg: &CFG) -> Result<(), Err> {
+pub fn analyze_fn(universe: &Universe, global_scope: &ScopedData, cfg: &CFG, fn_type: &FunctionType) -> Result<(), Err> {
 
-    let mut neighbors = cfg.graph().neighbors.directed(cfg.start()).collect();
+    let mut scope_stack = Vec::new();
     let mut current_scope = global_scope.clone();
-    let scope_stack = Vec::new();
 
-    scope_stack.push(current_scope.clone());
+    let mut to_check = cfg.after_start().unwrap();
 
+    loop {
+        match *node_w!(cfg, to_check) {
+            Node::End => break,
+            Node::Start | Node::BranchSplit | Node::BranchMerge | Node::LoopHead => {
+                to_check = cfg.next(to_check).unwrap();
+            }
+
+            Node::EnterScope => scope_stack.push(current_scope.clone()),
+            Node::ExitScope => current_scope = scope_stack.pop().expect("If CFG was generated properly and the graph is being walked correctly, there should be a scope to pop"),
+
+
+
+            Node::LocalVarDecl(var_decl) => {
+                let name = var_decl.var_name();
+                let var_id = var_decl.var_id();
+                let type_path = var_decl.type_path();
+
+                let type_id = current_scope.type_id(type_path)?;
+
+                current_scope.insert_var(name.to_owned(), var_id, type_id);
+
+                to_check = cfg.next(to_check).unwrap();
+            }
+
+            _ => unimplemented!(),
+        }
+    }
+
+    if scope_stack.len() != 0 {
+        panic!("Should be no left over scopes if CFG was generated properly and fully-walked.");
+    }
     unimplemented!();
 }
