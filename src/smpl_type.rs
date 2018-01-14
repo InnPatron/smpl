800684c32509c19174cc9a47304c1dcbc573e7ce
Random_Civvy <vomalexander@gmail.com>
2018-01-13 18:43:26 -0700

    Add type FnParameter.

--- src/smpl_type.rs
+++ src/smpl_type.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
+use std::cell::Cell;
 
-use semantic_ck::TypeId;
+use semantic_ck::{TypeId, VarId};
 use ast::Ident;
 
 #[derive(Clone, Debug, PartialEq)]
@@ -24,4 +25,33 @@ pub struct StructType {
 pub struct FunctionType {
     pub params: Vec<(Ident, TypeId)>,
     pub return_type: TypeId,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct FnParameter {
+    pub name: Ident,
+    pub param_type: TypeId,
+    var_id: Cell<Option<VarId>>,
+}
+
+impl FnParameter {
+    pub fn new(name: Ident, param_type: TypeId) -> FnParameter {
+        FnParameter {
+            name: name,
+            param_type: param_type,
+            var_id: Cell::new(None),
+        }
+    }
+
+    pub fn set_var_id(&self, id: VarId) {
+        match self.var_id.get() {
+            Some(previous) => panic!("Attempting to overwrite {} of parameter {} with {}", 
+                                     previous, self.name, id),
+            None => self.var_id.set(Some(id)),
+        }
+    }
+
+    pub fn var_id(&self) -> Option<VarId> {
+        self.var_id.get()
+    }
 }
