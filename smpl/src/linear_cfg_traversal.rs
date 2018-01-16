d0afa3260de4b0b7d91e6fc12b167921da992773
Random_Civvy <vomalexander@gmail.com>
2018-01-15 17:53:40 -0700

    Impl Traverser with stub for Node::Condition. Add trait Passenger which Traverser calls into while visitin nodes.

--- smpl/src/linear_cfg_traversal.rs
+++ smpl/src/linear_cfg_traversal.rs
@@ -1,13 +1,213 @@
-use petgraph::graph;
+use petgraph::graph::NodeIndex;
 use petgraph::Direction;
 use petgraph::visit::EdgeRef;
 
 use control_flow::*;
+use typed_ast::*;
 
-struct Traverse<'a> {
-    graph: &'a CFG,
+pub trait Passenger {
+    fn start(&mut self, id: NodeIndex);
+    fn end(&mut self, id: NodeIndex);
+    fn branch_merge(&mut self, id: NodeIndex);
+    fn loop_head(&mut self, id: NodeIndex);
+    fn loop_foot(&mut self, id: NodeIndex);
+    fn cont(&mut self, id: NodeIndex);
+    fn br(&mut self, id: NodeIndex);
+    fn enter_scope(&mut self, id: NodeIndex);
+    fn exit_scope(&mut self, id: NodeIndex);
+    fn local_var_decl(&mut self, id: NodeIndex, decl: &LocalVarDecl);
+    fn assignment(&mut self, id: NodeIndex, assign: &Assignment);
+    fn expr(&mut self, id: NodeIndex, expr: &Expr);
+    fn ret(&mut self, id: NodeIndex, expr: Option<&Expr>);
 }
 
-impl<'a> Traverse<'a> {
-    
+struct Traverser<'a, 'b> {
+    graph: &'a CFG,
+    passenger: &'b mut Passenger,
+}
+
+impl<'a, 'b> Traverser<'a, 'b> {
+
+
+    ///
+    /// Convenience function to get the next node in a linear sequence. If the current node has
+    /// multiple outgoing edge (such as Node::Condition, Node::Return, Node::Break, and
+    /// Node::Continue) or none (Node::End), return an error.
+    ///
+    fn next(&self, current: NodeIndex) -> NodeIndex {
+        let mut neighbors = self.graph.neighbors_out(current);
+
+        if neighbors.clone().count() != 1 {
+            panic!("CFG::next() only works when a Node has 1 neighbor");
+        } else {
+            neighbors.next().unwrap()
+        }
+    }
+
+    fn after_break(&self, id: NodeIndex) -> NodeIndex {
+
+        match *self.graph.node_weight(id) {
+            Node::Break(_) => (),
+            _ => panic!("Should only be given a Node::Break"),
+        }
+
+        let neighbors = self.graph.neighbors_out(id);
+        let neighbor_count = neighbors.clone().count();
+        
+        if neighbor_count == 2 {
+            let mut found_first = false;
+            for n in neighbors {
+                match *self.graph.node_weight(n) {
+                    Node::LoopFoot(_) => {
+                        if found_first {
+                            return n;
+                        } else {
+                            found_first = true;
+                        }
+                    }
+                    _ => return n,
+                }
+            }
+        } else if neighbor_count == 1 {
+
+        } else {
+            panic!("Node::Continue points to {} neighbors. Nodes should never point towards more than 2 neighbors but at least 1 (except Node::End).", neighbor_count);
+        }
+
+        unreachable!();
+    }
+
+    fn after_continue(&self, id: NodeIndex) -> NodeIndex {
+
+        match *self.graph.node_weight(id) {
+            Node::Continue(_) => (),
+            _ => panic!("Should only be given a Node::Continue"),
+        }
+
+        let mut neighbors = self.graph.neighbors_out(id);
+        let neighbor_count = neighbors.clone().count();
+
+        if neighbor_count == 2 {
+            let mut found_first = false;
+            for n in neighbors {
+                match *self.graph.node_weight(n) {
+                    Node::LoopHead(_) => {
+                        if found_first {    
+                            return n;
+                        } else {
+                            found_first = true;
+                        }
+                    }
+                    _ => return n,
+                }
+            }
+        } else if neighbor_count == 1 {
+            return neighbors.next().unwrap();
+        } else {
+            panic!("Node::Continue points to {} neighbors. Nodes should never point towards more than 2 neighbors but at least 1 (except Node::End).", neighbor_count);
+        }
+        
+        unreachable!();
+    }
+
+    fn after_loop_foot(&self, id: NodeIndex) -> NodeIndex {
+
+        let loop_id;
+        match *self.graph.node_weight(id) {
+            Node::LoopFoot(id) => loop_id = id,
+            _ => panic!("Should only be given a Node::LoopFoot"),
+        }
+
+        let neighbors = self.graph.neighbors_out(id);
+        let neighbor_count = neighbors.clone().count();
+        
+        if neighbor_count != 2 {
+            panic!("Loop foot should always be pointing to LoopHead and the next Node. Need two directed neighbors, found {}", neighbor_count);
+        }
+         
+        for n in neighbors {
+            match *self.graph.node_weight(n) {
+                Node::LoopHead(id) => {
+                    if loop_id != id {
+                        return n;
+                    }
+                }
+                _ => return n,
+            }
+        }
+        unreachable!();
+    }
+
+    fn visit_node(&mut self, current: NodeIndex) -> Option<NodeIndex> {
+        match *self.graph.node_weight(current) {
+            Node::End => {
+                self.passenger.end(current);
+                Some(self.next(current))
+            },
+
+            Node::Start => {
+                self.passenger.start(current);
+                Some(self.next(current))
+            }
+
+            Node::BranchMerge => {
+                self.passenger.branch_merge(current);
+                Some(self.next(current))
+            }
+
+            Node::LoopHead(_) => {
+                self.passenger.loop_head(current);
+                Some(self.next(current))
+            }
+
+            Node::LoopFoot(_) => {
+                self.passenger.loop_foot(current);
+                Some(self.after_loop_foot(current))
+            }
+
+            Node::Continue(_) => {
+                self.passenger.cont(current);
+                Some(self.after_continue(current))
+            }
+
+            Node::Break(_) => {
+                self.passenger.br(current);
+                Some(self.after_break(current))
+            }
+
+            Node::EnterScope => {
+                self.passenger.enter_scope(current);
+                Some(self.next(current))
+            }
+
+            Node::ExitScope => {
+                self.passenger.exit_scope(current);
+                Some(self.next(current))
+            }
+
+            Node::LocalVarDecl(ref decl) => {
+                self.passenger.local_var_decl(current, decl);
+                Some(self.next(current))
+            }
+
+            Node::Assignment(ref assign) => {
+                self.passenger.assignment(current, assign);
+                Some(self.next(current))
+            }
+
+            Node::Expr(ref expr) => {
+                self.passenger.expr(current, expr);
+                Some(self.next(current))
+            }
+
+            Node::Return(ref ret_expr) => {
+                self.passenger.ret(current, ret_expr.as_ref());
+                Some(self.next(current))
+            }
+
+            Node::Condition(ref condition) => {
+                unimplemented!()
+            }
+        }
+    }
 }
