407dbfef764071c687604f71e4fdb656c02d5513
Random_Civvy <vomalexander@gmail.com>
2018-01-15 17:08:39 -0700

    Add prototype linear_cfg_traversal module

---
+++ smpl/src/linear_cfg_traversal.rs
@@ -0,0 +1,13 @@
+use petgraph::graph;
+use petgraph::Direction;
+use petgraph::visit::EdgeRef;
+
+use control_flow::*;
+
+struct Traverse<'a> {
+    graph: &'a CFG,
+}
+
+impl<'a> Traverse<'a> {
+    
+}
