ebc2cafe85d0c759e1fa76a9dc39bd1ca32d69e8
Random_Civvy <vomalexander@gmail.com>
2018-01-15 21:19:20 -0700

    Print test names before compiling.

--- smpl-tests/test1.sh
+++ smpl-tests/test1.sh
@@ -6,8 +6,16 @@ if [ ! -d "./target" ]; then
 	mkdir ./target
 fi
 
+echo **if_tests**
+
+echo
+
 rm ./target/if_tests.rs
 ../target/debug/smplc --input if_tests.smpl --backend 0 --output ./target/
+
+echo **while_tests**
+
+echo
 
 rm ./target/while_tests.rs
 ../target/debug/smplc --input while_tests.smpl --backend 0 --output ./target/
