5e934fec05d0ce6efb402965f3ae0ed930a8aaaa
Random_Civvy <vomalexander@gmail.com>
2018-01-28 14:56:10 -0700

    Update tests to specify output file.

--- smpl-tests/test1.sh
+++ smpl-tests/test1.sh
@@ -11,25 +11,18 @@ echo **if_tests**
 echo
 
 rm ./target/if_tests.rs
-../target/debug/smplc --input if_tests.smpl --backend 0 --output ./target/
+../target/debug/smplc --input if_tests.smpl --backend 0 --output ./target/if_tests.rs
 
 echo **while_tests**
 
 echo
 
 rm ./target/while_tests.rs
-../target/debug/smplc --input while_tests.smpl --backend 0 --output ./target/
+../target/debug/smplc --input while_tests.smpl --backend 0 --output ./target/while_tests.rs
 
 echo **fn_tests**
 
 echo
 
 rm ./target/fn_tests.rs > /dev/null
-../target/debug/smplc --input fn_tests.smpl --backend 0 --output ./target/
-
-echo **sample_program**
-
-echo
-
-rm ./target/sample_program.rs > /dev/null
-../target/debug/smplc --input sample_program.smpl --backend 0 --output ./target/
+../target/debug/smplc --input fn_tests.smpl --backend 0 --output ./target/fn_tests.rs
