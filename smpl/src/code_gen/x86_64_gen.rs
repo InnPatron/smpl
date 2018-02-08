81f010d69ede6f7dda2887f916c8d520a93f3689
Random_Civvy <vomalexander@gmail.com>
2018-02-08 12:31:07 -0700

    Add struct layout generation.

--- smpl/src/code_gen/x86_64_gen.rs
+++ smpl/src/code_gen/x86_64_gen.rs
@@ -1,5 +1,74 @@
-pub struct X86_64Backend {
+use std::collections::HashMap;
 
+use ast::Ident;
+
+use analysis::smpl_type::*;
+use analysis::metadata::*;
+use analysis::{Universe, TypeId};
+
+const BYTE_ALIGNMENT: usize = 8;
+
+pub struct X86_64Backend {
+    type_sizes: HashMap<TypeId, usize>,
+    struct_layouts: HashMap<TypeId, StructLayout>,
+    byte_alignment: usize,
+}
+
+impl X86_64Backend {
+
+    pub fn new(universe: &Universe) -> X86_64Backend {
+        let std_sizes = vec![
+            // (universe.unit(), 0),
+            (universe.int(), 4),
+            (universe.float(), 4),
+            // (universe.string(), 8),
+            (universe.boolean(), 1),
+        ];
+
+
+        X86_64Backend {
+            type_sizes: std_sizes.into_iter().collect(),
+            struct_layouts: HashMap::new(),
+            byte_alignment: BYTE_ALIGNMENT,
+        }
+    }
+
+    fn size(&self, id: TypeId) -> usize {
+        *self.type_sizes.get(&id).unwrap()
+    }
+}
+
+struct StructLayout {
+    fields: HashMap<Ident, (usize, usize)>,     // (offset, size)
+    size: usize,
+}
+
+impl StructLayout {
+    pub fn new(backend: &X86_64Backend, universe: &Universe, 
+               struct_t: &StructType, meta: &StructMetadata) -> StructLayout {
+
+        let mut fields = HashMap::new();
+
+        let mut total_size = 0;
+        let mut current_offset = 0;
+        for field in meta.order() {
+            let field_type = *(struct_t.fields.get(field).unwrap());
+            let field_size = backend.size(field_type);
+            let padding = field_size % backend.byte_alignment;
+
+            let total_field_size = field_size + padding;
+
+            fields.insert(field.clone(), (current_offset, total_field_size));
+
+            current_offset += total_field_size;
+            total_size += total_field_size;
+        }
+        
+        StructLayout {
+            fields: fields,
+            size: total_size,
+        }
+    }
 }
 
 enum Location {
