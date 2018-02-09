c45aaf299b39108169cb8473eee1ecc6d0ff3a63
Random_Civvy <vomalexander@gmail.com>
2018-02-09 00:43:16 -0700

    Add code_gen::as::fn_id() (FnId -> String)

--- smpl/src/code_gen/asm/mod.rs
+++ smpl/src/code_gen/asm/mod.rs
@@ -3,10 +3,14 @@ pub mod x86_64_gen;
 mod x86_64_fn_gen;
 mod layout;
 
-use analysis::TypeId;
+use analysis::{TypeId, FnId};
 
 pub trait ASMBackend {
     fn layout(&self, id: TypeId) -> &self::layout::Layout;
 
     fn byte_alignment(&self) -> usize;
+}
+
+pub fn fn_id(id: FnId) -> String {
+    format!("fn_{}", id.raw())
 }
