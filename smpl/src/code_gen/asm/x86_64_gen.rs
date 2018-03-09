12dde59dc1914e3ccecb8154740b343e313a6e6e
Random_Civvy <vomalexander@gmail.com>
2018-03-08 18:01:54 -0700

    Add TRUE and FALSE constants. Add source comments.

--- smpl/src/code_gen/asm/x86_64_gen.rs
+++ smpl/src/code_gen/asm/x86_64_gen.rs
@@ -1,3 +1,25 @@
+/*
+ * Sources:
+ *
+ *  1) System V Application Binary Interface
+ *  AMD64 Architecture Processor Supplement
+ *  Draft Version 0.99.6a
+ *
+ *  Alias: AMD64_ABI
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+
+
 use std::collections::HashMap;
 
 use analysis::smpl_type::*;
@@ -7,9 +29,14 @@ use analysis::{Program, Universe, TypeId};
 use super::ASMBackend;
 use super::layout::*;
 
+// Source: AMD64_ABI 12
 pub const FLOAT_SIZE: usize = 4;
 pub const INT_SIZE: usize = 4;
 pub const BOOL_SIZE: usize = 1;
+
+// Source: AMD64_ABI 13
+pub const TRUE: i8 = 1;
+pub const FALSE: i8 = 0;
 
 pub const BYTE_ALIGNMENT: usize = 8;
 pub const POINTER_SIZE: usize = 8;
