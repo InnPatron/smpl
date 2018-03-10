8545869a5e41b55580cf938425bf8ace231c4624
Random_Civvy <vomalexander@gmail.com>
2018-03-09 19:20:11 -0700

    Prototype LocalAllocator.
    Allocates memory below RBP in continguous blocks.

--- smpl/src/code_gen/asm/x86_64_fn_gen.rs
+++ smpl/src/code_gen/asm/x86_64_fn_gen.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{VecDeque, HashMap};
 
 use petgraph::graph::NodeIndex;
 
@@ -343,6 +343,110 @@ impl<'a, 'b> Passenger<()> for x86_64FnGenerator<'a, 'b> {
     fn branch_end_false_path(&mut self, id: NodeIndex) -> Result<(), ()> {
         // Do nothing
         Ok(())
+    }
+}
+
+struct LocalAllocator {
+    storage: VecDeque<Block>,
+    map: HashMap<DataId, Vec<Block>>,
+    deepest_offset: usize,               // Used to track how much TOTAL stack space to allocate
+    block_size: usize,
+}
+
+impl LocalAllocator {
+    fn new(block_size: usize, blocks: usize) -> LocalAllocator {
+        let mut s = VecDeque::new();
+
+        // Begin at 1; stack grows DOWN
+        // Locals/temps are placed top-down but read down-top
+        // First variable will be at RBP-8
+        for i in 1..blocks+1 {
+            s.push_back(Block::new(i));
+        }
+
+        LocalAllocator {
+            storage: s,
+            map: HashMap::new(),
+            deepest_offset: 0,
+            block_size: block_size,
+        }
+    }
+
+    fn alloc<T: Into<DataId>>(&mut self, id: T, size: usize) -> DataLocation<Register> {
+        let block_min = size / self.block_size;
+        let excess = size % self.block_size;
+
+        let mut blocks_required = block_min;
+        if excess > 0 {
+            blocks_required += 1;
+        }
+
+        let mut start = None;
+        let mut to_allocate = 0;
+        let mut deepest_offset = 0;
+        for (current, ref current_block) in self.storage.iter().enumerate() {
+            if start.is_none() {
+                start = Some(current);
+                to_allocate = 1;
+                deepest_offset = current_block.offset;
+            } else if start.is_some() {
+                if to_allocate == blocks_required {
+                    break;
+                }
+
+                let previous_block = self.storage.get(current - 1).unwrap();
+                if previous_block.offset + 1 == current_block.offset {
+                    to_allocate += 1;
+                } else {
+                    start = Some(current);
+                    to_allocate = 1;
+                }
+
+                deepest_offset = current_block.offset;
+            }
+        }
+
+        if to_allocate != blocks_required {
+            panic!("Unable to allocate enough blocks (of size {}) to fit {}. Allocate more blocks?",
+            self.block_size, size);
+        }
+
+        if self.deepest_offset < deepest_offset {
+            self.deepest_offset = deepest_offset;
+        }
+
+        let start_block_index = start.unwrap();
+
+        // Start of variable in memory
+        let start_stack_offset = {
+            self.storage.get(start_block_index).unwrap().offset
+        };
+
+        // Remove memory blocks
+        let allocated = self.storage
+            .drain(start_block_index..start_block_index + blocks_required)
+            .collect::<Vec<_>>();
+
+        // Map allocated memory to a local / temporary
+        self.map.insert(id.into(), allocated);
+
+        DataLocation::Local(start_stack_offset * self.block_size)
+    }
+}
+
+struct Block {
+    offset: usize
+}
+
+impl Block {
+    fn new(offset: usize) -> Block {
+        Block {
+            offset: offset
+        }
+    }
+
+    fn offset(&self) -> usize {
+        self.offset
     }
 }
 
