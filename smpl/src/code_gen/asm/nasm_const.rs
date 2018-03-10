2c6270fcca832fdd78f91ab9a0876916fc2cae56
Random_Civvy <vomalexander@gmail.com>
2018-03-09 23:26:22 -0700

    Add constants for sizes types.

--- smpl/src/code_gen/asm/nasm_const.rs
+++ smpl/src/code_gen/asm/nasm_const.rs
@@ -7,7 +7,12 @@
  *
  *  Alias: AMD64_ABI
  *
+ *  2) Intel® 64 and IA-32 Architectures
+ *  Software Developer’s Manual
+ *  Combined Volumes:
+ *  1, 2A, 2B, 2C, 2D, 3A, 3B, 3C, 3D and 4
  *
+ *  Alias: INTEL_INSTR
  *
  *
  *
@@ -16,6 +21,42 @@
  *
  *
  */
+
+// Source: INTEL_INSTR 89 (vol. 1)
+#[derive(Clone, Copy, PartialEq)]
+pub enum SIZES {
+    QWORD,
+    DWORD,
+    WORD,
+    BYTE,
+}
+
+impl ::std::fmt::Display for SIZES {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        match *self {
+            SIZES::QWORD => write!(f, "QWORD"),
+            SIZES::DWORD => write!(f, "DWORD"),
+            SIZES::WORD => write!(f, "WORD"),
+            SIZES::BYTE => write!(f, "BYTE"),
+        }
+    }
+}
+
+impl SIZES {
+    fn size(s: SIZES) -> usize {
+        match s {
+            SIZES::QWORD => QWORD,
+            SIZES::DWORD => DWORD,
+            SIZES::WORD => WORD,
+            SIZES::BYTE => BYTE,
+        }
+    }
+}
+
+pub const QWORD: usize = 8;
+pub const DWORD: usize = 4;
+pub const WORD: usize = 2;
+pub const BYTE: usize = 1;
 
 // Source: AMD64_ABI 12
 pub const FLOAT_SIZE: usize = 4;
