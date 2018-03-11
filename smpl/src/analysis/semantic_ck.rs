8b7ec06ae263ff9c9e5a03d5fce24b2cacf6df33
Random_Civvy <vomalexander@gmail.com>
2018-03-11 02:38:30 -0700

    Fix move issue while generating struct type.

--- smpl/src/analysis/semantic_ck.rs
+++ smpl/src/analysis/semantic_ck.rs
@@ -248,7 +248,7 @@ fn generate_struct_type(universe: &Universe, scope: &ScopedData, struct_def: &St
             let f_type_path = &field.field_type;
             let field_type = scope.type_id(f_type_path)?;
             fields.insert(f_id, field_type);
-            field_map.insert(f_name, f_id);
+            field_map.insert(f_name.clone(), f_id);
             order.push(f_name);
         }
     } 
