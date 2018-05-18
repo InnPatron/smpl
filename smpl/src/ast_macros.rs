cf0995d95ee3494a9316754148d28951b4df6b8a
Alexander Vo <randomcivvy0121@gmail.com>
2018-05-18 14:23:56 -0700

    AST macros include dummy Span

--- smpl/src/ast_macros.rs
+++ smpl/src/ast_macros.rs
@@ -7,7 +7,8 @@ macro_rules! boolean {
 
     ($val: expr => Expr) => {{
         use ast::Expr;
-        Expr::Literal(boolean!($val))
+        use span::Span;
+        Expr::Literal(AstNode::new(boolean!($val), Span::new(0, 0)))
     }};
 
     ($val: expr => BoxExpr) => {{
@@ -24,7 +25,8 @@ macro_rules! int {
 
     ($int: expr => Expr) => {{
         use ast::Expr;
-        Expr::Literal(int!($int))
+        use span::Span;
+        Expr::Literal(AstNode::new(int!($int), Span::new(0, 0)))
     }};
 
     ($int: expr => BoxExpr) => {{
@@ -43,7 +45,8 @@ macro_rules! ident {
 
     ($ident: expr => Expr) => {{
         use ast::Expr;
-        Expr::Ident(ident!($ident))
+        use span::Span;
+        Expr::Ident(AstNode::new(ident!($ident), Span::new(0, 0)))
     }};
 
     ($ident: expr => BoxExpr) => {{
@@ -54,9 +57,10 @@ macro_rules! ident {
 #[allow(unused_macros)]
 macro_rules! path {
     ($($segment: expr),*) => {{
+        use span::Span;
         use ast::PathSegment as ASTPathSegment;
         let mut v = Vec::new();
-        $(v.push(ASTPathSegment::Ident(ident!($segment)));)*;
+        $(v.push(ASTPathSegment::Ident(AstNode::new(ident!($segment), Span::new(0, 0))));)*;
         Path(v)
     }};
 }
@@ -64,8 +68,9 @@ macro_rules! path {
 #[allow(unused_macros)]
 macro_rules! type_path {
     ($($segment: expr),*) => {{
+        use span::Span;
         let mut v = Vec::new();
-        $(v.push(ident!($segment));)*;
+        $(v.push(AstNode::new(ident!($segment), Span::new(0, 0)));)*;
         ModulePath(v)
     }};
 }
@@ -83,7 +88,8 @@ macro_rules! bin_expr {
 
     (($lhs: expr, $op: expr, $rhs: expr) => Expr) => {{
         use ast::Expr;
-        Expr::Bin(bin_expr!($lhs, $op, $rhs))
+        use span::Span;
+        Expr::Bin(AstNode::new(bin_expr!($lhs, $op, $rhs), Span::new(0, 0)))
     }};
 
     (($lhs: expr, $op: expr, $rhs: expr) => BoxExpr) => {{
