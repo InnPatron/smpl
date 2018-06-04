b1803f1d088a0e9042bc82bbaefcd5f7e546e54b
Alexander Vo <randomcivvy0121@gmail.com>
2018-06-03 18:44:12 -0700

    Bring 'used' data into scope

--- smpl/src/analysis/mod_resolver.rs
+++ smpl/src/analysis/mod_resolver.rs
@@ -14,6 +14,11 @@ use super::semantic_data::Module;
 use super::control_flow::CFG;
 use super::fn_analyzer::analyze_fn;
 
+struct RawProgram<'a> {
+    scopes: HashMap<ModuleId, ScopedData>,
+    raw_map: HashMap<&'a Ident, ModuleId>,
+}
+
 struct RawModData {
     name: AstNode<Ident>,
     id: ModuleId,
@@ -38,9 +43,66 @@ pub fn check_modules(program: &mut Program, modules: Vec<AstModule>) {
         let mut scope = program.universe().std_scope();
         map_internal_data(&mut scope, raw);
 
-        mapped_raw.insert(raw.name.data(), mod_id);
-        scopes.insert(mod_id, scope);
+        mapped_raw.insert(raw.name.data(), mod_id.clone());
+        scopes.insert(mod_id.clone(), scope);
     }
+
+    let mut raw_program = RawProgram {
+        scopes: scopes,
+        raw_map: mapped_raw
+    };
+
+    map_usings(&raw_data, &mut raw_program);
+}
+
+fn map_usings(raw_modules: &HashMap<ModuleId, RawModData>, raw_prog: &mut RawProgram) -> Result<(), Err> {
+    for (id, raw_mod) in raw_modules {
+        for use_decl in raw_mod.uses.iter() {
+            let import_name = use_decl.data().0.data();
+            let import_id = raw_prog.raw_map.get(import_name).ok_or(Err::UnresolvedUses(vec![use_decl.clone()]))?;
+
+            // Get imported module's types and functions
+            let (all_types, all_fns) = {
+                let imported_scope = raw_prog.scopes.get(import_id).unwrap();
+                let all_types = imported_scope.all_types()
+                                              .into_iter()
+                                              .map(|(path, id)| {
+                                                  let mut path = path.clone();
+                                                  path.0.insert(0, import_name.clone());
+                                                  
+                                                  (path, id.clone())
+                                              })
+                                              .collect::<HashMap<_, _>>();
+                let all_fns = imported_scope.all_fns()
+                                            .into_iter()
+                                            .map(|(path, id)| {
+                                                let mut path = path.clone();
+                                                path.0.insert(0, import_name.clone());
+                                                  
+                                                (path, id.clone())
+                                            })
+                                            .collect::<HashMap<_,_>>();
+
+                (all_types, all_fns)
+            };
+
+            let current_module_scope = raw_prog.scopes.get_mut(id).unwrap();
+
+            // Bring imported types into scope
+            for (path, imported) in all_types.into_iter() {
+                if current_module_scope.insert_type(path.clone().into(), imported).is_some() {
+                    panic!("Should not have overrwritten {}. Paths should be unique by prefixing with the originating module.", path);
+                }
+            }
+
+            // Bring imported functions into scope
+            for(path, imported) in all_fns.into_iter() {
+                current_module_scope.insert_fn(path, imported);
+            }
+        }
+    }
+
+    Ok(())
 }
 
 fn map_internal_data(scope: &mut ScopedData, raw: &RawModData) {
