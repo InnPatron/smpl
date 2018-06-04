fcf7ac79a9f99d4939d23d518ecd8748ad588710
Alexander Vo <randomcivvy0121@gmail.com>
2018-06-04 15:08:55 -0700

    Insert builtin functions into the Universe

--- smpl/src/analysis/mod_resolver.rs
+++ smpl/src/analysis/mod_resolver.rs
@@ -14,6 +14,8 @@ use super::semantic_data::Module;
 use super::control_flow::CFG;
 use super::fn_analyzer::analyze_fn;
 
+use feature::*;
+
 struct RawProgram<'a> {
     scopes: HashMap<ModuleId, ScopedData>,
     raw_map: HashMap<&'a Ident, ModuleId>,
@@ -78,6 +80,23 @@ pub fn check_modules(program: &mut Program, modules: Vec<AstModule>) -> Result<(
             let cfg = CFG::generate(program.universe(), fn_decl.clone(), &fn_type)?;
 
             program.universe_mut().insert_fn(fn_id, type_id, fn_type, cfg);
+            program.metadata_mut().insert_module_fn(mod_id.clone(), fn_decl.name.data().clone(), fn_id);
+        }
+
+        for (_, reserved_builtin) in raw_mod.reserved_builtins.iter() {
+            let fn_id = reserved_builtin.0;
+            let type_id = reserved_builtin.1;
+            let fn_decl = reserved_builtin.2.data();
+            let fn_type = generate_builtin_fn_type(program, 
+                                                   raw_program.scopes.get(mod_id).unwrap(), 
+                                                   fn_id, 
+                                                   reserved_builtin.2.data())?;
+
+            program.features_mut().add_feature(BUILTIN_FN);
+
+            program.universe_mut().insert_builtin_fn(fn_id, type_id, fn_type);
+
+            program.metadata_mut().insert_builtin(fn_id);
             program.metadata_mut().insert_module_fn(mod_id.clone(), fn_decl.name.data().clone(), fn_id);
         }
     }
@@ -162,6 +181,59 @@ fn generate_fn_type(program: &mut Program, scope: &ScopedData, fn_id: FnId, fn_d
             metadata.insert_function_param_ids(fn_id, Vec::with_capacity(0));
             Vec::with_capacity(0)
         }
+    };
+
+    Ok(FunctionType {
+        params: params,
+        return_type: ret_type,
+    })
+}
+
+fn generate_builtin_fn_type(program: &mut Program, scope: &ScopedData, fn_id: FnId, fn_def: &AstBuiltinFunction) -> Result<FunctionType, Err> {
+    let (universe, metadata, features) = program.analysis_context();
+    let ret_type = match fn_def.return_type {
+        Some(ref path) => {
+            let data = path.data();
+            let type_id = scope.type_id(universe, data.into())?;
+            fn_sig_type_scanner(universe, features, type_id);
+            type_id
+        }
+        None => universe.unit(),
+    };
+
+    let params = match fn_def.params {
+        BuiltinFnParams::Checked(ref params) => {
+            match *params {
+                Some(ref params) => {
+                    let mut typed_params = Vec::new();
+                    let mut param_metadata = Vec::new();
+                    for p in params.iter() {
+                        let param = p.data();
+                        let type_path = param.param_type.data();
+                        let type_id = scope.type_id(universe, type_path.into())?;
+                        typed_params.push(type_id);
+                        param_metadata.push(FunctionParameter::new(param.name.data().clone(), universe.new_var_id()));
+
+                        fn_sig_type_scanner(universe, features, type_id);
+                    }
+
+                    metadata.insert_function_param_ids(fn_id, param_metadata);
+
+                    typed_params
+                }
+                None => {
+                    metadata.insert_function_param_ids(fn_id, Vec::with_capacity(0));
+                    Vec::with_capacity(0)
+                }
+            }
+        }
+
+        BuiltinFnParams::Unchecked => {
+            metadata.insert_unchecked_builtin_params(fn_id);
+            features.add_feature(UNCHECKED_BUILTIN_FN_PARAMS);
+            Vec::with_capacity(0)
+        }
+
     };
 
     Ok(FunctionType {
