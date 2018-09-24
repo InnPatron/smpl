5292d793c3f8a4612b634ca3e2f03c584a55ed99
Alexander Vo <randomcivvy0121@gmail.com>
2018-09-24 16:23:57 -0700

    StackInfo holds the ExecutorState
    Remove GraphInfo

--- smpl/src/code_gen/interpreter/avm/vm.rs
+++ smpl/src/code_gen/interpreter/avm/vm.rs
@@ -30,11 +30,7 @@ pub struct StackInfo {
     pub func: FnId,
     pub func_env: Env,
     pub fn_context: FnContext,
-}
-
-pub enum GraphInfo {
-    Node(NodeIndex),
-    NodeExpr(NodeIndex, TmpIndex),
+    pub exec_state: ExecutorState,
 }
 
 pub struct ExecutionContext {
@@ -56,26 +52,33 @@ impl ExecutionContext {
     }
 }
 
+pub enum ExecutorState {
+    Fetch(NodeIndex),
+    Expr {
+        node: NodeIndex,
+        tmp_index: usize,
+        expr_phase: usize,
+    },
+    Eval(NodeIndex),
+}
+
 pub struct FnContext {
     pub fn_id: FnId,
     pub loop_heads: HashMap<LoopId, NodeIndex>,
     pub loop_result: HashMap<LoopId, bool>,
     pub previous_is_loop_head: bool,
     pub loop_stack: Vec<LoopId>,
-    pub graph_info: GraphInfo,
 }
 
 impl FnContext {
     fn new(program: &Program, fn_id: FnId) -> FnContext {
 
-        let info = GraphInfo::Node(program.universe().get_fn(fn_id).cfg().start());
         FnContext {
             fn_id: fn_id,
             loop_heads: HashMap::new(),
             loop_result: HashMap::new(),
             previous_is_loop_head: false,
             loop_stack: Vec::new(),
-            graph_info: info,
         }
     }
 
@@ -143,14 +146,4 @@ impl<'a> Executor<'a> {
 
         ExecResult::Pending
     }
-}
-
-enum ExecutorState {
-    Fetch(NodeIndex),
-    Expr {
-        node: NodeIndex,
-        tmp_index: usize,
-        expr_phase: usize,
-    },
-    Eval(NodeIndex),
 }
