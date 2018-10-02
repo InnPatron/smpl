76ded9822733ec62389e5064952fc9b634102447
Alexander Vo <randomcivvy0121@gmail.com>
2018-10-02 01:17:14 -0700

    Update interpreter tests to also test the AVM

--- smpl/src/code_gen/interpreter/vm_tests.rs
+++ smpl/src/code_gen/interpreter/vm_tests.rs
@@ -61,27 +61,22 @@ impl BuiltinFn for VarArgSum {
 
 #[test]
 fn interpreter_basic() {
-    let mod1 =
+    let result = setup_and_run!(
 "mod mod1;
 
 fn test(a: int, b: int) -> int {
 return a + b;
-}";
-
-    let modules = vec![parse_module(mod1).unwrap()];
-
-    let vm = VM::new(modules).unwrap();
+}",
+        "mod1",
+        "test",
+        Some(vec![Value::Int(5), Value::Int(7)]));
     
-    let fn_handle = vm.query_module("mod1", "test").unwrap().unwrap();
-
-    let result = vm.eval_fn_args(fn_handle, Some(vec![Value::Int(5), Value::Int(7)]));
-
     assert_eq!(Value::Int(12), result);
 }
 
 #[test]
 fn interpreter_struct() {
-    let mod1 =
+    let result = setup_and_run!(
 "mod mod1;
 
 struct T {
@@ -90,65 +85,47 @@ f: int
 
 fn test(a: int, b: int) -> T {
 return init T { f: a + b };
-}";
-
-    let modules = vec![parse_module(mod1).unwrap()];
-
-    let vm = VM::new(modules).unwrap();
-    
-    let fn_handle = vm.query_module("mod1", "test").unwrap().unwrap();
-
-    let result = vm.eval_fn_args(fn_handle, Some(vec![Value::Int(5), Value::Int(7)]));
+}",
+        "mod1",
+        "test",
+        Some(vec![Value::Int(5), Value::Int(7)]));
 
     let result = irmatch!(result; Value::Struct(s) => s.get_field("f").unwrap());
     let result = irmatch!(result; Value::Int(i) => i);
-
+        
     assert_eq!(12, result);
 }
 
 #[test]
 fn interpreter_builtin() {
-    let mod1 =
+    let result = setup_and_run!(
 "mod mod1;
 
 builtin fn add(a: int, b: int) -> int;
 
 fn test(a: int, b: int) -> int {
 return add(a, b);
-}";
-
-    let modules = vec![parse_module(mod1).unwrap()];
-
-    let mut vm = VM::new(modules).unwrap();
-    vm.insert_builtin("mod1", "add", Box::new(Add)).unwrap();
-    
-    let fn_handle = vm.query_module("mod1", "test").unwrap().unwrap();
-
-    let result = vm.eval_fn_args(fn_handle, Some(vec![Value::Int(5), Value::Int(7)]));
+}",
+        "mod1",
+        "test",
+        Some(vec![Value::Int(5), Value::Int(7)]));
 
     assert_eq!(Value::Int(12), result);
 }
 
 #[test]
 fn interpreter_builtin_unchecked_params() {
-    let mod1 =
+    let result = setup_and_run!(
 "mod mod1;
 
 builtin fn sum(UNCHECKED) -> int;
 
 fn test(a: int, b: int) -> int {
 return sum(a, b, 100, 2);
-}";
-
-    let modules = vec![parse_module(mod1).unwrap()];
-
-
-    let mut vm = VM::new(modules).unwrap();
-    vm.insert_builtin("mod1", "sum", Box::new(VarArgSum)).unwrap();
-    
-    let fn_handle = vm.query_module("mod1", "test").unwrap().unwrap();
-
-    let result = vm.eval_fn_args(fn_handle, Some(vec![Value::Int(5), Value::Int(7)]));
+}",
+        "mod1",
+        "test",
+        Some(vec![Value::Int(5), Value::Int(7)]));
 
     assert_eq!(Value::Int(114), result);
 }
@@ -176,19 +153,25 @@ return mod1::add(1, 2);
 
     let modules = vec![parse_module(mod1).unwrap(), parse_module(mod2).unwrap()];
 
-    let mut vm = VM::new(modules).unwrap();
+    let mut vm = VM::new(modules.clone()).unwrap();
+    let mut avm = AVM::new(modules).unwrap();
     vm.insert_builtin("mod1", "add", Box::new(Add)).unwrap();
+    avm.insert_builtin("mod1", "add", Box::new(Add)).unwrap();
     
     let fn_handle = vm.query_module("mod2", "test2").unwrap().unwrap();
+    let a_fn_handle = vm.query_module("mod2", "test2").unwrap().unwrap();
 
     let result = vm.eval_fn(fn_handle);
+    let a_result = avm.eval_fn_sync(fn_handle);
+
+    assert_eq!(result, a_result.unwrap());
 
     assert_eq!(Value::Int(3), result);
 }
 
 #[test]
 fn interpreter_field_access() {
-    let mod1 =
+    let result = setup_and_run!(
 "mod mod1;
 
 struct T {
@@ -203,22 +186,17 @@ t.f = t.f + 1;
 return t.f + 1;
 }
 
-";
-
-    let modules = vec![parse_module(mod1).unwrap()];
-
-    let vm = VM::new(modules).unwrap();
-    
-    let fn_handle = vm.query_module("mod1", "test").unwrap().unwrap();
-
-    let result = vm.eval_fn(fn_handle);
+",
+        "mod1",
+        "test",
+        None);
 
     assert_eq!(Value::Int(1337), result);
 }
 
 #[test]
 fn interpreter_array() {
-    let mod1 =
+    let result = setup_and_run!(
 "mod mod1;
 
 fn test() -> int {
@@ -227,22 +205,17 @@ let t: [int; 5] = [1, 2, 3, 4, 5];
 return t[0] + t[1] + t[2] + t[3] + t[4];
 }
 
-";
-
-    let modules = vec![parse_module(mod1).unwrap()];
-
-    let vm = VM::new(modules).unwrap();
-    
-    let fn_handle = vm.query_module("mod1", "test").unwrap().unwrap();
-
-    let result = vm.eval_fn(fn_handle);
+",
+        "mod1",
+        "test",
+        None);
 
     assert_eq!(Value::Int(1 + 2 + 3 + 4 + 5), result);
 }
 
 #[test]
 fn interpreter_fn_value() {
-    let mod1 =
+    let result = setup_and_run!(
 "mod mod1;
 
 fn test2(a: int) -> int {
@@ -255,22 +228,17 @@ let func: fn(int) -> int = test2;
 return func(210);
 }
 
-";
-
-    let modules = vec![parse_module(mod1).unwrap()];
-
-    let vm = VM::new(modules).unwrap();
-    
-    let fn_handle = vm.query_module("mod1", "test").unwrap().unwrap();
-
-    let result = vm.eval_fn(fn_handle);
+",
+        "mod1",
+        "test",
+        None);
 
     assert_eq!(Value::Int(420), result);
 }
 
 #[test]
 fn interpreter_optional_local_type_annotation() {
-    let mod1 =
+    let result = setup_and_run!(
 "mod mod1;
 
 fn test2(a: int) -> int {
@@ -283,22 +251,17 @@ let func = test2;
 return func(210);
 }
 
-";
-
-    let modules = vec![parse_module(mod1).unwrap()];
-
-    let vm = VM::new(modules).unwrap();
-    
-    let fn_handle = vm.query_module("mod1", "test").unwrap().unwrap();
-
-    let result = vm.eval_fn(fn_handle);
+",
+        "mod1",
+        "test",
+        None);
 
     assert_eq!(Value::Int(420), result);
 }
 
 #[test]
 fn interpreter_recursive_fn_call() {
-    let mod1 =
+    let result = setup_and_run!(
 "
 mod mod1;
 
@@ -309,21 +272,17 @@ if (i == 0) {
     return i + recurse(i - 1);
 }
 }
-";
-    let modules = vec![parse_module(mod1).unwrap()];
-
-    let vm = VM::new(modules).unwrap();
-
-    let fn_handle = vm.query_module("mod1", "recurse").unwrap().unwrap();
-
-    let result = vm.eval_fn_args(fn_handle, Some(vec![Value::Int(2)]));
+",
+    "mod1",
+    "recurse",
+    Some(vec![Value::Int(2)]));
 
     assert_eq!(Value::Int(3), result);
 }
 
 #[test]
 fn interpreter_mutually_recursive_fn_call() {
-    let mod1 =
+    let result = setup_and_run!(
 "
 mod mod1;
 
@@ -342,22 +301,17 @@ if (i == 0) {
     return recurse_a(i - 1);
 }
 }
-";
-
-    let modules = vec![parse_module(mod1).unwrap()];
-
-    let vm = VM::new(modules).unwrap();
-
-    let fn_handle = vm.query_module("mod1", "recurse_a").unwrap().unwrap();
-
-    let result = vm.eval_fn_args(fn_handle, Some(vec![Value::Int(1)]));
+",
+        "mod1",
+        "recurse_a",
+        Some(vec![Value::Int(1)]));
 
     assert_eq!(Value::Int(-5), result);    
 }
 
 #[test]
 fn interpreter_loaded_builtin() {
-    let mod1 =
+    let result = setup_and_run!(
 "
 mod mod1;
 use math;
@@ -366,21 +320,17 @@ fn test_floor() -> float {
 let f = math::floor(1.5);
 return f;
 }
-";
-    let modules = vec![parse_module(mod1).unwrap()];
-
-    let vm = VM::new(modules).unwrap();
-
-    let fn_handle = vm.query_module("mod1", "test_floor").unwrap().unwrap();
-
-    let result = vm.eval_fn(fn_handle);
+",
+        "mod1",
+        "test_floor",
+        None);
 
     assert_eq!(Value::Float(1.0), result);
 }
 
 #[test]
 fn interpreter_anonymous_fn_call() {
-    let mod1 =
+    let result = setup_and_run!(
 "
 mod mod1;
 
@@ -390,22 +340,17 @@ let func = fn (foo: int) -> int {
 };
 
 return func(10);
-}";
-
-    let mod1 = parse_module(mod1).unwrap();
-    
-    let vm = VM::new(vec![mod1]).unwrap();
-
-    let fn_handle = vm.query_module("mod1", "test").unwrap().unwrap();
-
-    let result = vm.eval_fn(fn_handle);
+}", 
+        "mod1",
+        "test",
+        None);
 
     assert_eq!(Value::Int(15), result);
 }
 
 #[test]
 fn interpreter_anonymous_fn_arg() {
-    let mod1 =
+    let result = setup_and_run!(
 "mod mod1;
 
 fn test2(func: fn(int) -> int) -> int {
@@ -418,22 +363,17 @@ let func = fn (foo: int) -> int {
 };
 
 return test2(func);
-}";
-
-    let mod1 = parse_module(mod1).unwrap();
-    
-    let vm = VM::new(vec![mod1]).unwrap();
-
-    let fn_handle = vm.query_module("mod1", "test").unwrap().unwrap();
-
-    let result = vm.eval_fn(fn_handle);
+}",
+        "mod1",
+        "test",
+        None);
 
     assert_eq!(Value::Int(15), result);
 }
 
 #[test]
 fn interpreter_fn_piping() {
-    let mod1 =
+    let result = setup_and_run!(
 "
 mod mod1;
 
@@ -443,15 +383,10 @@ return i + a;
 
 fn test() -> int {
 return add(0, 1) |> add(1) |> add(1) |> add(2);
-}";
-
-    let mod1 = parse_module(mod1).unwrap();
-
-    let vm = VM::new(vec![mod1]).unwrap();
-
-    let fn_handle = vm.query_module("mod1", "test").unwrap().unwrap();
-
-    let result = vm.eval_fn(fn_handle);
+}",
+        "mod1",
+        "test",
+        None);
 
     assert_eq!(Value::Int(5), result);
 }
