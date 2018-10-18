0bdf4894b68e2bb954cc5f360654480d27cd1fbe
Alexander Vo <randomcivvy0121@gmail.com>
2018-10-18 00:33:36 -0700

    Add dependency on failure and failure_derive

--- smpl/src/lib.rs
+++ smpl/src/lib.rs
@@ -3,6 +3,9 @@ extern crate irmatch;
 extern crate itertools;
 extern crate petgraph;
 extern crate strfmt;
+extern crate failure;
+#[macro_use]
+extern crate failure_derive;
 
 mod feature;
 mod err;
