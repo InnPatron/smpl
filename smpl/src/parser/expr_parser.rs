5322e27a57cae577134801a6c6740a24cd5203c5
Alexander Vo <randomcivvy0121@gmail.com>
2018-11-11 04:43:54 -0700

    Add precedence methods

--- smpl/src/parser/expr_parser.rs
+++ smpl/src/parser/expr_parser.rs
@@ -20,3 +20,43 @@ pub fn expr(tokens: &mut BufferedTokenizer, delim_token: &[Delimiter], min_prece
 pub fn parse_primary(tokens: &mut BufferedTokenizer) -> ParseErr<Expr> {
     unimplemented!()
 }
+
+fn bin_op_precedence(op: &BinOp) -> u64 {
+    use self::BinOp::*;
+    match op {
+        Add => 2,
+        Sub => 2,
+        Mul => 3,
+        Div => 3,
+        Mod => 3,
+
+        LogicalAnd => 6,
+        LogicalOr => 6,
+        GreaterEq => 4,
+        LesserEq => 4,
+        Greater => 4,
+        Lesser => 4,
+        Eq => 5,
+        InEq => 5,
+    }
+}
+
+fn is_left_associative(op: &BinOp) -> bool {
+    use self::BinOp::*;
+    match op {
+        Add => true,
+        Sub => true,
+        Mul => true,
+        Div => true,
+        Mod => true,
+
+        LogicalAnd => true,
+        LogicalOr => true,
+        GreaterEq => true,
+        LesserEq => true,
+        Greater => true,
+        Lesser => true,
+        Eq => true,
+        InEq => true,
+    }
+}
