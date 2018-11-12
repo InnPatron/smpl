296472b4ab7af4b28c6a82e4e1d18f06a03a2932
Alexander Vo <randomcivvy0121@gmail.com>
2018-11-11 18:03:18 -0700

    Add tests parse_paren_expr() and parse_nested_paren_expr()

--- smpl/src/parser/parser_tests.rs
+++ smpl/src/parser/parser_tests.rs
@@ -518,6 +518,27 @@ struct TestStruct {
              0).unwrap();
     }
 
+    #[test]
+    fn parse_paren_expr() {
+        let input = "(1 + 10) * (4 / 3)";
+        let mut input = buffer_input(input);
+        let lhs = parse_primary(&mut input).unwrap();
+        expr(&mut input, 
+             lhs,
+             &[],
+             0).unwrap();
+    }
+
+    #[test]
+    fn parse_nested_paren_expr() {
+        let input = "((1 + 10) * (4 / (3)))";
+        let mut input = buffer_input(input);
+        let lhs = parse_primary(&mut input).unwrap();
+        expr(&mut input, 
+             lhs,
+             &[],
+             0).unwrap();
+    }
 
     #[test]
     fn parse_annotate_struct() {
