651b1702fc5b0cc5beb7a28bc1f304454db2aeb4
Alexander Vo <randomcivvy0121@gmail.com>
2018-12-13 01:24:03 -0700

    Add macros for checking argument counts

--- smpl/src/code_gen/interpreter/err.rs
+++ smpl/src/code_gen/interpreter/err.rs
@@ -23,3 +23,116 @@ pub enum ExpectedArgCount {
     #[fail(display = "Expected == {}", _0)]
     Exact(usize),
 }
+
+// Checks for no arguments
+#[macro_export]
+macro_rules! no_args {
+    ($args: expr) => {{
+        use crate::code_gen::interpreter::err::*;
+        match $args {
+            Some(args) => {
+                Err(InternalError::InvalidArgCount(args.len(), ExpectedArgCount::Exact(0)))
+            }
+
+            None => {
+                Ok(None)
+            }
+        }
+    }}
+}
+
+#[macro_export]
+macro_rules! exact_args {
+    ($exact: expr, $args: expr) => {{
+        use crate::code_gen::interpreter::err::*;
+        match $args {
+            Some(args) => {
+                if args.len() != $exact {
+                    Err(InternalError::InvalidArgCount(args.len(), ExpectedArgCount::Exact($exact)))
+                } else {
+                    Ok(args)
+                }
+            },
+
+            None => {
+                // Assume $exact != 0 to make types nicer
+                // Use no_args!() instead
+                assert!($exact != 0);
+                Err(InternalError::InvalidArgCount(0, ExpectedArgCount::Exact($exact)))
+            }
+        }
+    }}
+}
+
+// Checks for 1+ args
+#[macro_export]
+macro_rules! min_args {
+    ($min: expr, $args: expr) => {{
+        use crate::code_gen::interpreter::err::*;
+        match $args {
+            Some(args) => {
+                if args.len() < $min {
+                    Err(InternalError::InvalidArgCount(args.len(), ExpectedArgCount::Min($min)))
+                } else {
+                    Ok(args)
+                }
+            },
+
+            None => {
+                // min = 0 is the same as not checking the arguments
+                assert!($min != 0);
+                Err(InternalError::InvalidArgCount(0, ExpectedArgCount::Min($min)))
+            }
+        }
+    }}
+}
+
+// Checks for <= max args
+#[macro_export]
+macro_rules! max_args {
+    ($max: expr, $args: expr) => {{
+        use crate::code_gen::interpreter::err::*;
+        match $args {
+            Some(args) => {
+                if args.len() > $max {
+                    Err(InternalError::InvalidArgCount(args.len(), ExpectedArgCount::Max($max)))
+                } else {
+                    Ok(Some(args))
+                }
+            }
+
+            None => Ok(None)
+        }
+    }}
+}
+
+// Checks for [min, max] args
+#[macro_export]
+macro_rules! arg_range_inclusive {
+    // Assume min_inclusive is greater than 1
+    ($min_inclusive: expr, $max_inclusive: expr, $args: expr) => {{ 
+        use crate::code_gen::interpreter::err::*;
+        match $args {
+            Some(args) => {
+                if $min_inclusive <= args.len() && $max_inclusive >= args.len() {
+                    Ok(args)
+                } else {
+                    Err(InternalError::InvalidArgCount(args.len(), 
+                                                       ExpectedArgCount::Range($min_inclusive, 
+                                                                                $max_inclusive)))
+                }
+            }
+
+            None => {
+                // Assume min_inclusive is greater than 0
+                // If min_inclusive == 0, use max_args instead
+                if $min_inclusive == 0 {
+                    panic!("Minimum inclusive is 0. Use max_args!() instead");
+                }
+                Err(InternalError::InvalidArgCount(0, 
+                                                   ExpectedArgCount::Range($min_inclusive, 
+                                                                            $max_inclusive)))
+            }
+        }
+    }}
+}
