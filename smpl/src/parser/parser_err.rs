9a495e791f7591ba1602ec496bc91345552f1a9d
Alexander Vo <randomcivvy0121@gmail.com>
2018-12-15 01:33:18 -0700

    Add struct ParserState

--- smpl/src/parser/parser_err.rs
+++ smpl/src/parser/parser_err.rs
@@ -28,3 +28,33 @@ impl From<SpannedError> for ParserError {
         ParserError::TokenizerError(e)
     }
 }
+
+pub struct ParserState {
+    state: String,
+    substate: Option<String>,
+}
+
+impl ParserState {
+    pub fn new_state(state: &str) -> ParserState {
+        ParserState {
+            state: state.to_string(),
+            substate: None,
+        }
+    }
+
+    pub fn substate(self, substate: &str) -> ParserState {
+        ParserState {
+            substate: Some(substate.to_string()),
+            .. self
+        }
+    }
+}
+
+impl std::fmt::Display for ParserState {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self.substate {
+            Some(ref sub) => write!(f, "[{}::{}]", self.state, sub),
+            None => write!(f, "[{}]", self.state),
+        }
+    }
+}
