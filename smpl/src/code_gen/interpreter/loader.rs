db433318e047059edc6bf564862d06c95c8f34b6
Alexander Vo <randomcivvy0121@gmail.com>
2018-12-19 03:00:30 -0700

    Replace the old loading routine with Loader.

--- smpl/src/code_gen/interpreter/loader.rs
+++ smpl/src/code_gen/interpreter/loader.rs
@@ -99,21 +99,3 @@ impl StdOptions {
         }
     }
 }
-
-pub fn include(mut modules: Vec<Module>) -> Vec<Module> {
-    log::include(&mut modules);
-    convert::include(&mut modules);
-    math::include(&mut modules);
-    err::include(&mut modules);
-    str::include(&mut modules);
-
-    modules
-}
-
-pub fn load<MAP: BuiltinMap>(vm: &mut MAP) {
-    log::add(vm);
-    convert::add(vm);
-    math::add(vm);
-    err::add(vm);
-    str::add(vm);
-}
