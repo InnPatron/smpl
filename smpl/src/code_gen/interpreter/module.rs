2cd35e9666e9f9659c98842a9376cd11a568a878
Alexander Vo <randomcivvy0121@gmail.com>
2018-12-22 04:01:22 -0700

    VmModule stores builtins as (String, BuiltinFn). Save validation for VM creation

--- smpl/src/code_gen/interpreter/module.rs
+++ smpl/src/code_gen/interpreter/module.rs
@@ -1,36 +1,27 @@
-use std::collections::HashMap;
-
 use crate::code_gen::interpreter::BuiltinFn;
 use crate::module::ParsedModule;
 use crate::analysis::ModuleId;
 
 pub struct VmModule {
     pub parsed: ParsedModule,
-    pub builtins: HashMap<String, BuiltinFn>,
+    pub builtins: Vec<(String, BuiltinFn)>,
 }
 
 impl VmModule {
     pub fn new(m: ParsedModule) -> VmModule {
-        VmModule::with_builtins(m, HashMap::new())
+        VmModule::with_builtins(m, Vec::new())
     }
 
-    pub fn with_builtins(m: ParsedModule, v: HashMap<String, BuiltinFn>) -> VmModule {
+    pub fn with_builtins(m: ParsedModule, v: Vec<(String, BuiltinFn)>) -> VmModule {
         VmModule {
             parsed: m,
             builtins: v,
         }
     }
 
-    pub fn add_builtin(mut self, fn_name: &str, builtin: BuiltinFn) -> Result<VmModule, ()> {
-        if self.builtins
-            .insert(fn_name.to_string(), builtin)
-            .is_none() {
-
-            Ok(self)
-        } else {
-            // TODO: Error
-            Err(())
-        }
+    pub fn add_builtin(mut self, fn_name: &str, builtin: BuiltinFn) -> VmModule {
+        self.builtins.push((fn_name.to_string(), builtin));
+        self
     }
 
     pub fn id(&self) -> ModuleId {
