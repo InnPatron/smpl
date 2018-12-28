1e33eff89e9b57e0ec2a7d444450eb2c2addd9fa
Alexander Vo <randomcivvy0121@gmail.com>
2018-12-28 14:48:35 -0700

    Implement struct type constructor generation

--- smpl/src/analysis/type_cons.rs
+++ smpl/src/analysis/type_cons.rs
@@ -1,10 +1,10 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-use crate::ast::Ident;
+use crate::ast::{Struct, Ident, ModulePath, TypeAnnotation};
 
-use super::semantic_data::{FieldId, TypeId};
+use super::semantic_data::{FieldId, TypeId, Program, ScopedData};
 use super::smpl_type::*;
-use super::error::ApplicationError;
+use super::error::{AnalysisError, TypeError, ApplicationError};
 
 pub enum TypeCons {
 
@@ -256,4 +256,82 @@ impl TypeCons {
             }
         }
     }
+}
+
+// TODO: Store type constructors in Program
+pub fn generate_struct_type_cons(
+    program: &mut Program,
+    scope: &ScopedData,
+    struct_def: &Struct,
+) -> Result<Vec<FieldId>, AnalysisError> {
+    let (universe, _metadata, _features) = program.analysis_context();
+
+    // Check no parameter naming conflicts
+    let mut type_parameter_map = HashMap::new();
+    match struct_def.type_params {
+        Some(ref params) => {
+            for p in params.params.iter() {
+                if type_parameter_map.contains_key(p.data()) {
+                    return Err(TypeError::ParameterNamingConflict {
+                        ident: p.data().clone(),
+                    }.into());
+                } else {
+                    type_parameter_map.insert(p.data(), universe.new_type_id());
+                }
+            }
+        }
+
+        None => (),
+    }
+
+    // Generate the constructor
+    let mut fields = HashMap::new();
+    let mut field_map = HashMap::new();
+    let mut order = Vec::new();
+    if let Some(ref body) = struct_def.body.0 {
+        for field in body.iter() {
+            let f_id = universe.new_field_id();
+            let f_name = field.name.data().clone();
+            let f_type_path = &field.field_type;
+            let path_data = f_type_path.data();
+
+            let field_type = match struct_def.type_params {
+
+                // TODO: Recursively generate type constructor generators for fields
+                Some(ref params) => {
+                    unimplemented!()
+                }
+
+                // No type parameters, behave as if concrete type constructor
+                None => scope.type_id(universe, path_data.into())?,
+            };
+
+            // Map field to type constructor
+            fields.insert(f_id, field_type);
+
+            if field_map.contains_key(&f_name) {
+                return Err(TypeError::FieldNamingConflict {
+                    ident: f_name.clone(),
+                }.into());
+            } else {
+                field_map.insert(f_name, f_id);
+            }
+
+            order.push(f_id);
+        }
+    }
+
+    // TODO: Insert type constructor
+    let type_cons = TypeCons::Record {
+        name: struct_def.name.data().clone(),
+        fields: fields,
+        field_map: field_map,
+        type_params: type_parameter_map
+            .values()
+            .map(|id| id.clone())
+            .collect(),
+    };
+    
+
+    Ok(order)
 }
