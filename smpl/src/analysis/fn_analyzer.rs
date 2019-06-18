5b80cb50242e6dc2b4a14864bba5603d7aa17417
Alexander Vo <randomcivvy0121@gmail.com>
2019-06-18 02:40:23 -0700

    Function analyzer ignores ExitScope
    ExitScope are part of BasicBlock now

--- smpl/src/analysis/fn_analyzer.rs
+++ smpl/src/analysis/fn_analyzer.rs
@@ -145,8 +145,6 @@ fn return_check_id(cfg: &CFG, id: NodeIndex) -> Result<Option<Vec<NodeIndex>>, A
 
         Node::BranchMerge(_) => Ok(Some(cfg.before_branch_merge(id))),
 
-        Node::ExitScope => Ok(Some(vec![cfg.previous(id)])),
-
         _ => return Err(ControlFlowError::MissingReturn.into()),
     }
 }
