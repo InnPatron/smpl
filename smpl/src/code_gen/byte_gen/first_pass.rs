5da145208acbb396db59d119879b2747db578a53
Alexander Vo <randomcivvy0121@gmail.com>
2019-07-07 00:35:04 -0700

    Add helper methods to BranchFrame and FirstPass.
    BranchFrame.default is now an Option
    FirstPass keeps track of all frames

--- smpl/src/code_gen/byte_gen/first_pass.rs
+++ smpl/src/code_gen/byte_gen/first_pass.rs
@@ -23,8 +23,39 @@ enum State {
 }
 
 struct BranchFrame {
-    conditions: Vec<(Vec<PartialInstruction>, Vec<PartialInstruction>)>,
-    default: Vec<PartialInstruction>
+    conditions: Vec<(Vec<PartialInstruction>, Option<Vec<PartialInstruction>>)>,
+    default: Option<Vec<PartialInstruction>>
+}
+
+impl BranchFrame {
+    fn new() -> BranchFrame {
+        BranchFrame {
+            conditions: Vec::new(),
+            default: None
+        }
+    }
+
+    fn push_condition(&mut self, condition: Vec<PartialInstruction>) {
+        self.conditions.push((condition, None));
+    }
+
+    fn push_true_body(&mut self, true_body: Vec<PartialInstruction>) {
+        let (condition, body) = self.conditions.last_mut().unwrap();
+
+        if body.is_some() {
+            panic!("Attempting to double set the true body");
+        } 
+
+        *body = Some(true_body);
+    }
+
+    fn set_default(&mut self, default_body: Vec<PartialInstruction>) {
+        if self.default.is_some() {
+            panic!("Attempting to double set the default branch.");
+        }
+
+        self.default = Some(default_body);
+    }
 }
 
 struct FirstPass<'a> {
@@ -32,7 +63,7 @@ struct FirstPass<'a> {
     instructions: Vec<PartialInstruction>,
     loops: HashMap<LoopId, Vec<PartialInstruction>>,
     branches: HashMap<BranchingId, BranchFrame>,
-    current_frame: Vec<PartialInstruction>,
+    frames: Vec<Vec<PartialInstruction>>,
     states: Vec<State>,
 }
 
@@ -50,6 +81,26 @@ impl<'a> FirstPass<'a> {
 
     fn push_state(&mut self, state: State) { 
         self.states.push(state);
+    }
+
+    fn get_branch_frame_mut(&mut self, id: BranchingId) -> &mut BranchFrame {
+        self.branches.get_mut(&id).expect("Expected a branch frame. Found none.")
+    }
+
+    fn push_to_current_frame(&mut self, instr: PartialInstruction) {
+        let current_frame = self.frames.last_mut().expect("Expected a frame. Found none.");
+        current_frame.push(instr);
+    }
+
+    fn new_frame(&mut self) {
+        self.frames.push(Vec::new());
+    }
+
+    fn consume_current_frame(&mut self) -> Vec<PartialInstruction> {
+        let current_frame = self.frames.pop().expect("Expected a frame. Found none.");
+        self.frames.push(Vec::new());
+
+        current_frame
     }
 }
 
