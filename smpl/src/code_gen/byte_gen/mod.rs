bc26c050d3a6d0fe63c8a06968e3a1584c1f0a39
Alexander Vo <randomcivvy0121@gmail.com>
2019-09-12 23:13:22 -0700

    Reexport items from byte_code.rs

--- smpl/src/code_gen/byte_gen/mod.rs
+++ smpl/src/code_gen/byte_gen/mod.rs
@@ -3,3 +3,14 @@ mod byte_expr;
 mod first_pass;
 mod second_pass;
 mod third_pass;
+
+pub use byte_code::{
+    Instruction,
+    JumpTarget,
+    RelJumpTarget,
+    Location,
+    FieldAccess,
+    Arg,
+    Struct,
+    StructField,
+};
