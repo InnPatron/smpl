3ffc594ac3cadc2d6ee1c17b47bf4446f2c4bdb3
Alexander Vo <randomcivvy0121@gmail.com>
2019-09-14 17:44:17 -0700

    Begin type-api

--- smpl/src/analysis/type_api.rs
+++ smpl/src/analysis/type_api.rs
@@ -1,5 +1,70 @@
 use super::*;
+use super::type_cons::{ AbstractType, TypeCons };
 
-pub fn apply(universe: &Universe, type_id: TypeId, parameters: Vec<Type>) -> Type {
+#[derive(Debug, Clone)]
+pub enum TypeAPIError {
+    TyParamArity {
+        expected: usize,
+        found: usize,
+    },
+
+    AppOnPrimitive(PrimitiveKind),
+}
+
+#[derive(Debug, Clone)]
+pub enum PrimitiveKind {
+    String,
+    Int,
+    Float,
+    Bool,
+    Unit,
+}
+
+pub fn apply(universe: &Universe, type_id: TypeId, parameters: Vec<Type>) 
+    -> Result<Type, TypeAPIError> {
+
+    let type_cons = universe
+        .get_type_cons(type_id)
+        .expect(&format!("Unknown type constructor for {}", type_id));
+
+    match type_cons {
+        TypeCons::UncheckedFunction {
+            type_params,
+            return_type,
+        } => {
+
+        }
+
+        TypeCons::Function {
+            type_params,
+            parameters,
+            return_type,
+        } => {
+
+        }
+
+        TypeCons::Array {
+            element_type,
+            size,
+        } => {
+
+        }
+
+        TypeCons::Record {
+            type_id,
+            type_params,
+            fields,
+            field_map,
+        } => {
+
+        }
+
+        TypeCons::Int => return Err(TypeAPIError::AppOnPrimitive(PrimitiveKind::Int)),
+        TypeCons::Float => return Err(TypeAPIError::AppOnPrimitive(PrimitiveKind::Float)),
+        TypeCons::String => return Err(TypeAPIError::AppOnPrimitive(PrimitiveKind::String)),
+        TypeCons::Bool => return Err(TypeAPIError::AppOnPrimitive(PrimitiveKind::Bool)),
+        TypeCons::Unit => return Err(TypeAPIError::AppOnPrimitive(PrimitiveKind::Unit)),
+    }
+
     unimplemented!()
 }
