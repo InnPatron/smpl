0f9e6356d16fb1ba3f316be6bf54f94d1d5bd4f8
Alexander Vo <randomcivvy0121@gmail.com>
2019-09-15 20:54:14 -0700

    Env's can store and erase temporaries separately

--- smpli/src/env.rs
+++ smpli/src/env.rs
@@ -7,17 +7,25 @@ use super::value::Value;
 #[derive(Debug, Clone)]
 pub struct Env {
     env: HashMap<String, Rc<RefCell<Value>>>,
+    tmp_store: HashMap<String, Rc<RefCell<Value>>>,
 }
 
 impl Env {
     pub fn new() -> Env {
         Env {
             env: HashMap::new(),
+            tmp_store: HashMap::new(),
         }
     }
 
     pub fn map_value(&mut self, name: String, value: Value) -> Option<Value> {
         self.env
+            .insert(name, Rc::new(RefCell::new(value)))
+            .map(|rc| rc.borrow().clone())
+    }
+
+    pub fn map_tmp(&mut self, name: String, value: Value) -> Option<Value> {
+        self.tmp_store
             .insert(name, Rc::new(RefCell::new(value)))
             .map(|rc| rc.borrow().clone())
     }
@@ -28,5 +36,17 @@ impl Env {
 
     pub fn ref_value(&self, name: &str) -> Option<Rc<RefCell<Value>>> {
         self.env.get(name).map(|r| r.clone())
+    }
+
+    pub fn get_tmp(&self, name: &str) -> Option<Value> {
+        self.tmp_store.get(name).map(|r| (*r.borrow()).clone())
+    }
+
+    pub fn ref_tmp(&self, name: &str) -> Option<Rc<RefCell<Value>>> {
+        self.tmp_store.get(name).map(|r| r.clone())
+    }
+
+    pub fn wipe_tmps(&mut self) {
+        self.tmp_store.clear();
     }
 }
