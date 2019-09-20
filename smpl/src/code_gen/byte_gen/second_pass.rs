fdc75b6eb28b5c4d7a26502ddd9b1080c30ba56b
Alexander Vo <randomcivvy0121@gmail.com>
2019-09-19 23:03:54 -0700

    Adjust loop generation again

--- smpl/src/code_gen/byte_gen/second_pass.rs
+++ smpl/src/code_gen/byte_gen/second_pass.rs
@@ -70,8 +70,8 @@ impl SecondPass {
                     // Append the loop skip instruction
                     // Skips the body if the condition results in FALSE
                     // TODO(alex): Add check to ensure body length within u64 size?
-                    let skip_loop_rel_target: i64 = (body.len() as i64) + 1;
-                    // body-size + 1 to skip over the looper jump
+                    let skip_loop_rel_target: i64 = (body.len() as i64) + 2;
+                    // body-size + 2 to skip over the looper jump
                     let skip_loop_instr = Instruction::RelJumpNegateCondition(
                         RelJumpTarget::new(skip_loop_rel_target),
                         result_arg.clone()
@@ -85,7 +85,7 @@ impl SecondPass {
                     // Unconditionally jumps to start of condition instructions
                     // Loop skip instruction should jump to directly AFTER this instruction
                     let looper_rel_target: i64 = 
-                        -((body.len() as i64) + (condition.len() as i64)) - 1;
+                        -((body.len() as i64) + (condition.len() as i64)) - 2;
                     let loop_instr = Instruction::RelJump(
                         RelJumpTarget::new(looper_rel_target)
                     );
