5b9284f56a1965ca2de61723b35f2c72e750364a
Alexander Vo <randomcivvy0121@gmail.com>
2019-09-25 20:39:46 -0700

    Begin writing resolve_types()

--- smpl/src/analysis/type_resolver.rs
+++ smpl/src/analysis/type_resolver.rs
@@ -1,10 +1,40 @@
+use std::collections::HashMap;
 
 use super::type_cons::*;
 use super::error::TypeError;
-use super::semantic_data::ScopedData;
+use super::semantic_data::{TypeParamId, ScopedData, Universe };
 
-pub fn resolve_types(scoped_data: &ScopedData, lhs: &AbstractType, 
-    rhs: &AbstractType) -> Result<(), TypeError> {
+#[derive(Clone)]
+pub struct TypingContext {
+    map: HashMap<TypeParamId, AbstractType>,
+}
+
+pub fn resolve_types(universe: &Universe, scoped_data: &ScopedData, typing_context: TypingContext,
+    synthesis: &AbstractType, constraint: &AbstractType) -> Result<(AbstractType, TypingContext), TypeError> {
+
+    let synthesis = synthesis.apply(universe, scoped_data).unwrap();
+    let constraint = constraint.apply(universe, scoped_data).unwrap();
+
+    match (synthesis, constraint) {
+        (AbstractType::Record {
+            type_id: synth_type_id,
+            abstract_field_map: synth_afm,
+        }, AbstractType::Record {
+            type_id: constraint_type_id,
+            abstract_field_map: AbstractFieldMap {
+                fields: constraint_fields,
+                ..
+            }, 
+        }) => {
+            unimplemented!()
+        }
+
+        (AbstractType::App { .. }, _) | (_, AbstractType::App { .. }) => {
+            unreachable!("No AbstractType::App after apply");
+        }
+
+        _ => unimplemented!(),
+    }
 
     unimplemented!();
 }
