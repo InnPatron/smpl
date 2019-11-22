0d7f038a6d8147d008ad59d7f0478aef8b0eba5a
Alexander Vo <randomcivvy0121@gmail.com>
2019-11-21 23:44:45 -0700

    Make smpl::program pub. Add smpl::prelude

--- smpl/src/lib.rs
+++ smpl/src/lib.rs
@@ -17,18 +17,21 @@ mod ast;
 #[macro_use]
 mod analysis;
 mod code_gen;
-mod program;
+pub mod program;
 mod span;
 
 pub mod parser;
 pub mod error;
-pub use self::module::{ParsedModule, UnparsedModule};
 
-pub use self::parser::parse_module;
 pub use crate::analysis::{FnId, ModuleId, TypeId};
 
 pub use crate::analysis::metadata;
 
 pub use crate::code_gen::byte_gen;
 
-pub use crate::program::{CompilableFn, Program};
+pub mod prelude {
+    pub use crate::module::{ParsedModule, UnparsedModule};
+    pub use crate::parser::parse_module;
+    pub use crate::program::{CompilableFn, Program};
+    pub use crate::analysis::{FnId, ModuleId, TypeId};
+}
