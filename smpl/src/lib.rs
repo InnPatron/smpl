0b43152f29d58a0e35886c79aafd75e223f855fe
Alexander Vo <randomcivvy0121@gmail.com>
2019-11-23 20:29:21 -0700

    Add crate doc blurb

--- smpl/src/lib.rs
+++ smpl/src/lib.rs
@@ -1,3 +1,20 @@
+/*!
+
+  Core library to parse, analyze, and compile the SMPL language.
+
+  * This crate does **NOT** come with the means to execute SMPL code. 
+    * Use a crate like [smpli](https://crates.io/crates/smpli) for a runtime/std instead
+
+  # Using the Crate
+
+  1. Find your SMPL module code and create an `UnparsedModule`
+  2. Convert your collection of `UnparsedModule` into a `Program` by:
+     * Preparse using `parser::parse_module()` and calling `Program::from_parsed()`
+     * Call `Program::from_unparsed()` directly
+  3. Pass off the `Program` to a code generator (such as in `smpl::byte_gen`)
+     * Code generators will have full access to type information, control flow, metadata, etc.
+*/
+
 #[macro_use]
 extern crate irmatch;
 extern crate failure;
