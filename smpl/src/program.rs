afba7cf7b2cad96787cfee6d0b6dc188e951c09a
Alexander Vo <randomcivvy0121@gmail.com>
2019-11-23 20:20:35 -0700

    Add Program::from_unparsed(). Rename Program::new() to Program::from_parsed()
    Some smpl::program doc comments

--- smpl/src/program.rs
+++ smpl/src/program.rs
@@ -6,21 +6,49 @@ use crate::analysis::{
     check_program, AnonymousFunction, FnId, Function,
     Program as AnalyzedProgram, TypingContext, CFG,
 };
-use crate::module::ParsedModule;
+use crate::module::{UnparsedModule, ParsedModule};
+use crate::error::Error;
 
+///
+/// Represents a collection of fully parsed and analyzed SMPL modules.
+///
 pub struct Program {
     program: AnalyzedProgram,
 }
 
 impl Program {
-    pub fn new(
-        modules: Vec<ParsedModule>,
-    ) -> Result<Program, AnalysisError> {
+
+    /// 
+    /// Create a new `Program` from an iterator of unparsed SMPL modules.
+    ///
+    pub fn from_unparsed<'a, I>(modules: I) -> Result<Program, Error> 
+    where I: Iterator<Item=UnparsedModule<'a>> {
+        use crate::parser::parse_module;
+
+        let parsed = modules
+            .into_iter()
+            .map(|p| parse_module(p))
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Program::from_parsed(parsed.into_iter())
+            .map_err(|e| e.into())
+    }
+
+    /// 
+    /// Create a new `Program` from an iterator of pre-parsed SMPL modules.
+    ///
+    pub fn from_parsed<I>(
+        modules: I,
+    ) -> Result<Program, AnalysisError>
+    where I: Iterator<Item=ParsedModule> {
         Ok(Program {
-            program: check_program(modules)?,
+            program: check_program(modules.collect())?,
         })
     }
 
+    ///
+    /// Access to this `Program`'s metadata collected during static analysis.
+    ///
     pub fn metadata(&self) -> &crate::metadata::Metadata {
         &self.program.metadata()
     }
