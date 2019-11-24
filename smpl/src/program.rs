7cda85f32eb9565a78b7ce927909e616dadd7f26
Alexander Vo <randomcivvy0121@gmail.com>
2019-11-23 22:38:51 -0700

    Add abstraction CompilableModule that wraps SMPL modules for crate consumers

--- smpl/src/program.rs
+++ smpl/src/program.rs
@@ -5,6 +5,7 @@ use crate::analysis::error::AnalysisError;
 use crate::analysis::{
     check_program, AnonymousFunction, FnId, Function,
     Program as AnalyzedProgram, TypingContext, CFG,
+    ModuleId, Module as AnalyzedModule,
 };
 use crate::module::ParsedModule;
 
@@ -25,57 +26,96 @@ impl Program {
         &self.program.metadata()
     }
 
-    pub fn compilable_fns<'a>(
-        &'a self,
-    ) -> impl Iterator<Item = (FnId, CompilableFn)> + 'a {
+    pub fn compilable_modules(&self) ->
+        impl Iterator<Item = CompilableModule> {
+
         self.program
-            .all_fns()
-            .filter(|(_, f)| {
-                match f {
-                    Function::SMPL(_) => true,
-                    Function::Anonymous(_) => true,
-                    _ => false
-                }
-            })
-            .map(|(f_id, f)| {
-                match f {
-                    Function::SMPL(f) => {
-                        let c_fn = CompilableFn {
-                            cfg: f.cfg(),
-                            typing_context: f.analysis_context().typing_context(),
-                        };
-                        (f_id, c_fn)
-                    },
+            .universe()
+            .all_modules()
+            .map(move |(_, mod_id)| {
+                let module = self.program.universe().get_module(mod_id);
 
-                    Function::Anonymous(AnonymousFunction::Reserved(_)) => {
-                        panic!("All anonymous functions should be resolved after analysis");
-                    }
-
-                    Function::Anonymous(AnonymousFunction::Resolved {
-                        ref cfg,
-                        ref analysis_context,
-                        ..
-                    }) => {
-                        let c_fn = CompilableFn {
-                            cfg: cfg.clone(),
-                            typing_context: analysis_context.typing_context()
-                        };
-
-                        (f_id, c_fn)
-                    },
-
-                    _ => unreachable!(),
+                CompilableModule {
+                    program: &self.program,
+                    module: module,
                 }
             })
     }
 }
 
+pub struct CompilableModule<'a> {
+    program: &'a AnalyzedProgram,
+    module: &'a AnalyzedModule,
+}
+
+impl<'a> CompilableModule<'a> {
+
+    pub fn id(&self) -> ModuleId {
+        self.module.module_id()
+    }
+
+    pub fn source(&self) -> &crate::module::ModuleSource {
+        self.module.source()
+    }
+
+    pub fn compilable_fns(&self) -> 
+        impl Iterator<Item = CompilableFn> {
+
+        self.module
+            .owned_fns()
+            .iter()
+            .map(move |fn_id| {
+                let func = self.program.universe().get_fn(fn_id.clone());
+                CompilableModule::to_compilable_fn(fn_id.clone(), func)
+            })
+    }
+
+    fn to_compilable_fn(f_id: FnId, f: &Function) -> CompilableFn {
+        match f {
+            Function::SMPL(f) => {
+                let c_fn = CompilableFn {
+                    fn_id: f_id.clone(),
+                    cfg: f.cfg(),
+                    typing_context: f.analysis_context().typing_context(),
+                };
+                c_fn
+            },
+
+            Function::Anonymous(AnonymousFunction::Reserved(_)) => {
+                panic!("All anonymous functions should be resolved after analysis");
+            }
+
+            Function::Anonymous(AnonymousFunction::Resolved {
+                ref cfg,
+                ref analysis_context,
+                ..
+            }) => {
+                let c_fn = CompilableFn {
+                    fn_id: f_id.clone(),
+                    cfg: cfg.clone(),
+                    typing_context: analysis_context.typing_context()
+                };
+
+                c_fn
+            },
+
+            _ => unreachable!(),
+        }
+    }
+}
+
 pub struct CompilableFn<'a> {
+    fn_id: FnId,
     cfg: Rc<RefCell<CFG>>,
     typing_context: &'a TypingContext,
 }
 
 impl<'a> CompilableFn<'a> {
+
+    pub fn fn_id(&self) -> FnId {
+        self.fn_id.clone()
+    }
+
     pub(crate) fn cfg(&self) -> &Rc<RefCell<CFG>> {
         &self.cfg
     }
