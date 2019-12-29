cd950294c00c366597832bd1f83dd4433ad4deba
Alexander Vo <randomcivvy0121@gmail.com>
2019-12-28 21:47:00 -0700

    Collect SMPL function parameter metadata (type-less)

--- smpl/src/analysis/metadata_collectors.rs
+++ smpl/src/analysis/metadata_collectors.rs
@@ -17,8 +17,41 @@ pub(super) fn collect_metadata(metadata: &mut Metadata, raw_program: &Analyzable
 pub(super) fn collect_metadata_post(metadata: &mut Metadata, universe: &Universe)
     -> Result<(), MetadataError> {
 
+    smpl_function_parameters(metadata, universe);
 
     Ok(())
+}
+
+///
+/// Collects parameter order (name, local variable ID) for SMPL functions.
+///
+/// Parameter order of builtin functions are NOT collected.
+///
+fn smpl_function_parameters(metadata: &mut Metadata, universe: &Universe) {
+    use super::semantic_data::{Function, SMPLFunction, AnonymousFn};
+    use super::metadata::FunctionParameter;
+
+    for (fn_id, func) in universe.all_fns() {
+        match func {
+            Function::SMPL(SMPLFunction {
+                ref analysis_context,
+                ..
+            }) | Function::Anonymous(AnonymousFn {
+                ref analysis_context,
+                ..
+            }) => {
+                metadata.insert_function_param_ids(
+                    fn_id,
+                    analysis_context.param_order()
+                        .iter()
+                        .map(|(ident, id)| FunctionParameter::new(ident.clone(), id.clone()))
+                        .collect()
+                );
+            }
+
+            _ => (),
+        }
+    }
 }
 
 
