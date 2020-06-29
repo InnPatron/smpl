6eff6a4cfd14f8a640cdca28fe4a4013b0af264c
Alexander Vo <randomcivvy0121@gmail.com>
2020-06-28 21:30:28 -0700

    Begin rewriting tests with an expected AST

--- smpl/src/parser/new_parser_tests.rs
+++ smpl/src/parser/new_parser_tests.rs
@@ -4,6 +4,9 @@ use crate::new_ast::*;
 use crate::expr_ast::*;
 use crate::module::*;
 use crate::parser::buffer_input;
+use crate::span::*;
+use crate::ast_node::*;
+use crate::typable_ast::*;
 
 macro_rules! include_test {
     ($file_name: expr) => {{
@@ -21,6 +24,18 @@ macro_rules! test_parse_module {
             let mod1 = parse_module(mod1)
                 .expect("Module did not parse correctly");
         }
+    };
+
+    ($name: ident, $expected: expr) => {
+        #[test]
+        fn $name() {
+            let mod1 = include_test!(concat!(stringify!($name), ".smpl"));
+
+            let mod1 = parse_module(mod1)
+                .expect("Module did not parse correctly");
+
+            assert_eq!(mod1, $expected);
+        }
     }
 }
 
@@ -31,9 +46,37 @@ fn parse_module(input: &str) -> ParserResult<Module> {
     new_parser::module(&mut tokens)
 }
 
-test_parse_module!(empty_module_declared);
+test_parse_module!(empty_module_declared,
+    module!("mod1" => vec![])
 
-test_parse_module!(basic_fn);
+);
+
+test_parse_module!(basic_fn,
+    module!("mod1" => vec![
+        decl!(FN => Function {
+            name: dummy_node!(ident!("foo")),
+            params: vec![],
+            return_type: Some(dummy_node!(UNTYPED => type_ann!(PATH => Type))),
+            body: dummy_node!(UNTYPED => block!(EMPTY)),
+            annotations: vec![],
+            type_params: None,
+            where_clause: None,
+        }),
+
+        decl!(FN => Function {
+            name: dummy_node!(ident!("bar")),
+            params: vec![
+                fn_param!(x => type_ann!(PATH => int)),
+                fn_param!(y => type_ann!(PATH => int)),
+            ],
+            return_type: Some(dummy_node!(UNTYPED => type_ann!(PATH => Type))),
+            body: dummy_node!(UNTYPED => block!(EMPTY)),
+            annotations: vec![],
+            type_params: None,
+            where_clause: None,
+        })
+    ])
+);
 
 test_parse_module!(basic_structs);
 
