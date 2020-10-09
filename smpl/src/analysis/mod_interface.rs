87175b07dc83f3649ce1e49a1845300c2ebdfa23
Alexander Vo <randomcivvy0121@gmail.com>
2020-10-08 20:48:10 -0700

    Implement foreign module interface merging

--- smpl/src/analysis/mod_interface.rs
+++ smpl/src/analysis/mod_interface.rs
@@ -31,6 +31,7 @@ macro_rules! try_insert {
     }
 }
 
+
 enum ExportState {
     Export(Option<Name>),
     NoExport,
@@ -55,6 +56,103 @@ pub fn module_interfaces(dep_graph: &mut DepGraph) -> InterfaceResult<ModInterfa
     };
 
     Ok(mi)
+}
+
+///
+/// Merge a foreign module's interface based on the export statements of the current module
+///
+fn merge_foreign_interface(foreign_mod_name: &Ident, current_module: &ParsedModule, current_mi: &mut ModInterface, foreign_mi: &ModInterface) -> InterfaceResult<()> {
+
+    let mut all = false;
+    // Keys are the foreign keys
+    // ExportStates are the local re-export states
+    let mut local_reexport_states: HashMap<Ident, ExportState> = HashMap::new();
+    for decl in current_module.module.top_levels.iter() {
+        match decl {
+            DeclStmt::Export(ref export) => {
+                match export.node() {
+                    ExportDecl::ExportAll {
+                        from_module: Some(ref from_module),
+                        ref except,
+                    } => {
+                        if from_module.node() != foreign_mod_name {
+                            continue;
+                        }
+
+                        all = true;
+                        populate_excepts(&mut local_reexport_states,
+                            except.iter().map(|node| &node.node().0))?;
+                    }
+
+                    ExportDecl::ExportItems {
+                        from_module: Some(ref from_module),
+                        ref items
+                    } => {
+                        if from_module.node() != foreign_mod_name {
+                            continue;
+                        }
+
+                        populate_additions(&mut local_reexport_states,
+                            items.iter().map(|node| &node.node().0))?;
+                    }
+
+                    _ => continue,
+                }
+            }
+
+            _ => continue,
+        }
+    }
+
+    let foreign_keys_to_search = if all  {
+        Box::new(foreign_mi.0.iter().map(|(i, _)| i)) as Box<dyn Iterator<Item=_>>
+    } else {
+        Box::new(local_reexport_states.iter().map(|(i, _)| i)) as Box<dyn Iterator<Item=_>>
+    };
+
+    // Loop through all the foreign keys to search
+    //   Add to the current module interface depending on the all flag and the export state
+    //   The new items are foreign Name::ModuleItem's
+    let current_mi: &mut HashMap<Ident, Name> = &mut current_mi.0;
+    for foreign_key in foreign_keys_to_search {
+
+        let foreign_item = match foreign_mi.0.get(foreign_key) {
+            Some(foreign_item) => foreign_item,
+            None => todo!("Foreign module does not have '{}' in its interface", foreign_key),
+        };
+        match local_reexport_states.get(foreign_key) {
+
+            // Not re-exporting all symbols from the foreign module and
+            //   no export state was found
+            None if !all => continue,
+
+
+            // Explicitly not exporting this foreign item
+            Some(ExportState::NoExport) => continue,
+
+
+            // No export state but re-exporting all anyways
+            // NOTE: order matters
+            None => {
+                if let Some(_conflict) = current_mi.insert(foreign_key.clone(), foreign_item.clone()) {
+                    todo!("Conflict with key '{}'", foreign_key);
+                }
+            }
+
+            Some(ExportState::Export(ref alias)) => {
+
+                let key = match alias {
+                    Some(alias) => get_item(alias),
+                    None => foreign_key,
+                };
+                if let Some(_conflict) = current_mi.insert(key.clone(), foreign_item.clone()) {
+                    todo!("Conflict with key '{}'", key);
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
 
 ///
@@ -250,4 +348,15 @@ fn populate_additions<'a, T>(state: &mut HashMap<Ident, ExportState>, list: T) -
     }
 
     Ok(())
+}
+
+fn get_item(n: &Name) -> &Ident {
+    match n {
+        Name::Name(ref i) => i,
+        Name::ModuleItem {
+            ref item,
+            ..
+        } => item,
+        _ => panic!("No item for {}", n),
+    }
 }
