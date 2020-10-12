3dfaf894f51e72e5ba9579583fa37c6a52720d47
Alexander Vo <randomcivvy0121@gmail.com>
2020-10-11 23:23:24 -0700

    Add error variants

--- smpl/src/analysis/error.rs
+++ smpl/src/analysis/error.rs
@@ -1,5 +1,5 @@
 use crate::module::ModuleSource;
-use crate::ast::Ident;
+use crate::ast::{Name, Ident};
 
 #[derive(Debug, Clone)]
 pub enum AnalysisError {
@@ -11,5 +11,17 @@ pub enum AnalysisError {
         dependency: Ident,
     },
     CyclicDependencies(Vec<(Ident, ModuleSource)>),
+    ConflictingModItems {
+        c1_item: Name,
+        c1_module: Ident,
+        c1_module_source: ModuleSource,
+        c2_item: Name,
+        c2_module: Ident,
+        c2_module_source: ModuleSource,
+    },
+    UnknownModItem {
+        module: Ident,
+        item: Ident,
+    }
 
 }
