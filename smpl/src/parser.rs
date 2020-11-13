5f63b028d45de280989850fcd0fdd4b28bf03105
Alexander Vo <randomcivvy0121@gmail.com>
2020-11-12 23:44:08 -0800

    Add parser test 'dot_fn_calls' that parses for a chain of dot accesses and function calls

--- smpl/src/parser.rs
+++ smpl/src/parser.rs
@@ -7,3 +7,24 @@ lalrpop_mod!(pub pparser, "/parser/pparser.rs");
 
 pub use self::pparser::ModuleParser;
 pub use self::tokens::LiteralData;
+
+#[cfg(test)]
+mod tests {
+    use super::lexer::Tokenizer;
+    use super::pparser::ModuleParser;
+
+    #[test]
+    fn dot_fn_calls() {
+        let input = "
+            mod dot_fn_calls;
+
+            fn foo() {
+                a.b.c().d.e.f();
+            }
+        ";
+
+        let lexer = Tokenizer::new(input);
+        let parser = ModuleParser::new();
+        parser.parse(lexer).unwrap();
+    }
+}
