1c795e689104f8455fb395731b7e28dc7cc11186
Alexander Vo <randomcivvy0121@gmail.com>
2020-11-14 02:09:02 -0800

    Add Source

--- smpl/src/lib.rs
+++ smpl/src/lib.rs
@@ -7,3 +7,21 @@ mod ast_node;
 mod expr_ast;
 mod parser;
 mod span;
+
+use std::fmt;
+use std::path::PathBuf;
+
+#[derive(Clone, Debug)]
+pub enum Source {
+    File(PathBuf),
+    Anonymous(String),
+}
+
+impl fmt::Display for Source {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Source::File(ref path) => write!(f, "{}", path.display()),
+            Source::Anonymous(ref hint) => write!(f, "{}", hint),
+        }
+    }
+}
